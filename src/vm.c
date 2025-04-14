baf7d84d5456e236e8dfa9c69556d734d38b932f
Archil Bolkvadze <a.bolkvadze@gmail.com>
2025-04-14 23:06:20 +0400

    trace execution

--- src/vm.c
+++ src/vm.c
@@ -1,5 +1,6 @@
 #include "vm.h"
 #include "common.h"
+#include "debug.h"
 #include <stdio.h>
 
 VM vm;
@@ -12,6 +13,9 @@ static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
   for (;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+    disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
     uint8_t instruction;
     switch (instruction = READ_BYTE()) {
     case OP_CONSTANT: {
