f30bc161bba9419703d29c06227a5701c02a5cbf
Archil Bolkvadze <a.bolkvadze@gmail.com>
2025-04-15 12:44:53 +0400

    add operator not

--- src/compiler.c
+++ src/compiler.c
@@ -171,6 +171,9 @@ static void unary() {
   parsePrecedence(PREC_UNARY);
   // Emit the operator instruction.
   switch (operatorType) {
+  case TOKEN_BANG:
+    emitByte(OP_NOT);
+    break;
   case TOKEN_MINUS:
     emitByte(OP_NEGATE);
     break;
@@ -191,7 +194,7 @@ ParseRule rules[] = {
     [TOKEN_SEMICOLON]     = {NULL,    NULL,   PREC_NONE},
     [TOKEN_SLASH]         = {NULL,    binary, PREC_FACTOR},
     [TOKEN_STAR]          = {NULL,    binary, PREC_FACTOR},
-    [TOKEN_BANG]          = {NULL,    NULL,   PREC_NONE},
+    [TOKEN_BANG]          = {unary,   NULL,   PREC_NONE},
     [TOKEN_BANG_EQUAL]    = {NULL,    NULL,   PREC_NONE},
     [TOKEN_EQUAL]         = {NULL,    NULL,   PREC_NONE},
     [TOKEN_EQUAL_EQUAL]   = {NULL,    NULL,   PREC_NONE},
