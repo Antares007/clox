40318fc313ef30c959d39974d01c946fb587573e
Archil Bolkvadze <a.bolkvadze@gmail.com>
2025-04-17 13:51:42 +0400

    Function Calls

--- src/compiler.c
+++ src/compiler.c
@@ -134,7 +134,10 @@ static int emitJump(uint8_t instruction) {
   emitByte(0xff);
   return currentChunk()->count - 2;
 }
-static void emitReturn() { emitByte(OP_RETURN); }
+static void emitReturn() {
+  emitByte(OP_NIL);
+  emitByte(OP_RETURN);
+}
 static uint8_t makeConstant(Value value) {
   int constant = addConstant(currentChunk(), value);
   if (constant > UINT8_MAX) {
@@ -273,6 +276,20 @@ static void defineVariable(uint8_t global) {
 
   emitBytes(OP_DEFINE_GLOBAL, global);
 }
+static uint8_t argumentList() {
+  uint8_t argCount = 0;
+  if (!check(TOKEN_RIGHT_PAREN)) {
+    do {
+      expression();
+      if (argCount == 255) {
+        error("Can't have more than 255 arguments.");
+      }
+      argCount++;
+    } while (match(TOKEN_COMMA));
+  }
+  consume(TOKEN_RIGHT_PAREN, "Expect ')' after arguments.");
+  return argCount;
+}
 static void and_(bool canAssign) {
   int endJump = emitJump(OP_JUMP_IF_FALSE);
 
@@ -322,6 +339,10 @@ static void binary(bool canAssign) {
   default:
     return; // Unreachable.
   }
+}
+static void call(bool canAssign) {
+  uint8_t argCount = argumentList();
+  emitBytes(OP_CALL, argCount);
 }
 static void literal(bool canAssign) {
   switch (parser.previous.type) {
@@ -401,7 +422,7 @@ static void unary(bool canAssign) {
 }
 // clang-format off
 ParseRule rules[] = {
-    [TOKEN_LEFT_PAREN]    = {grouping,NULL,   PREC_NONE},
+    [TOKEN_LEFT_PAREN]    = {grouping,call,   PREC_CALL},
     [TOKEN_RIGHT_PAREN]   = {NULL,    NULL,   PREC_NONE},
     [TOKEN_LEFT_BRACE]    = {NULL,    NULL,   PREC_NONE},
     [TOKEN_RIGHT_BRACE]   = {NULL,    NULL,   PREC_NONE},
